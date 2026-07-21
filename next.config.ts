170e9e9edecef3b70e1a9889b1a84c24b572aa0b
Максим Зимовец <maksimzimovec@Maksims-MacBook-Air.local>
2026-07-21 13:21:26 +0300

    Add nodemailer to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@seald-io/nedb'],
+  serverExternalPackages: ['@seald-io/nedb', 'nodemailer'],
   async headers() {
     return [
       {
